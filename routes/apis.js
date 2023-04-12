66e290180ede8290644a4258dc208bb166168cfd
unknown <amirnaderie@gmail.com>
2023-04-12 12:07:16 +0330

    improve 1

--- routes/apis.js
+++ routes/apis.js
@@ -1,5 +1,6 @@
 const config = require("config");
 const express = require("express");
+var path = require("path");
 const router = express.Router();
 
 router.get("/",async (req, res) => {
@@ -19,5 +20,18 @@ router.get("/",async (req, res) => {
   }
 });
     
+router.get("/getfile",async (req, res) => {
+  try {
 
+    let filePath = path.join(
+      "./PostmanFolder/" ,
+      `${req.query.filename}.json`
+    );
+   
+    res.download(filePath);
+  } catch (error) {
+    res.status(error.response.status).send('خطا در دانلود فایل');
+    
+  }
+});
 module.exports = router;
